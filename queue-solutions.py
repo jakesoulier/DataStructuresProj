01bf7ad6bd8e059daf5312a0b5f45527afe45078
jakesoulier <97404870+jakesoulier@users.noreply.github.com>
2022-06-30 14:15:11 -0600

    Create queue-solutions.py

---
+++ queue-solutions.py
@@ -0,0 +1,22 @@
+class Queue():
+    def __init__(self):
+        self.items = []
+    def enqueue(self, item):
+        self.items.append(item)
+    def dequeue(self):
+        self.items.pop()
+    def ifEmpty(self):
+        return self.items == []
+    def size(self):
+        return len(self.items)
+
+q = Queue()
+q.enqueue('name')
+q.enqueue('date')
+q.enqueue('age')
+q.dequeue()
+q.ifEmpty()
+len = q.size()
+print(len)
+q.enqueue(5)
+print(q.items)
